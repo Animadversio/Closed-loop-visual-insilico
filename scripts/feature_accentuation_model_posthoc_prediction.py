7f6dc9c3aab0f60c0331d2f47176c5202747e9ca
Binxu Wang <binxu_wang@hms.harvard.edu>
2025-10-16 02:53:43 -0400

    add posthoc prediction for nat images script

--- scripts/feature_accentuation_model_posthoc_prediction.py
+++ scripts/feature_accentuation_model_posthoc_prediction.py
@@ -45,48 +45,50 @@ from neural_regress.posthoc_analysis_lib import parse_accentuated_filenames_to_d
 # Check that all predictors can be loaded! 
 config_root = r"/n/holylabs/LABS/alvarez_lab/Everyone/Accentuate_VVS/accentuation_configs"
 acc_stim_root = r"/n/holylabs/LABS/alvarez_lab/Everyone/Accentuate_VVS/accentuation_outputs"
-subject_id = "red_20250428-20250430"
-# subject_id = "paul_20250428-20250430"
-subject_ids = ["leap_250426-250501", "three0_250426-250501", "venus_250426-250429"]
+
+subject_ids = ["red_20250428-20250430",  "paul_20250428-20250430", "leap_250426-250501", "three0_250426-250501", "venus_250426-250429"]
 for subject_id in subject_ids:
     posthoc_dir = f"/n/holylabs/LABS/alvarez_lab/Lab/VVS_Accentuation/Encoding_models/{subject_id}/posthoc_model_predict"
     os.makedirs(posthoc_dir, exist_ok=True)
     # %% [markdown]
     # ### Construct the stimuli set
-    target_subfolder = sorted(glob.glob(join(acc_stim_root, f"*{subject_id}*_accentuation")))
-    # List all png files in each subfolder (one level deep)
-    all_png_files = {}
-    for subfolder in target_subfolder:
-        subfolder_name = os.path.basename(subfolder)
-        png_files = glob.glob(join(subfolder, "*.png"))
-        # Sort the files to ensure consistent order
-        png_files.sort()
-        all_png_files[subfolder_name] = png_files
-        print(f"Found {len(png_files)} PNG files in {subfolder_name}")
-        # Print first few files as example
-        for file in png_files[:5]:
-            print(f"  - {os.path.basename(file)}")
-        if len(png_files) > 5:
-            print(f"  - ... and {len(png_files)-5} more files")
-        print()
+    encoding_stim_dir = r"/n/holylabs/LABS/alvarez_lab/Lab/VVS_Accentuation/Stimuli/encodingstimuli_apr2025"
+    df_encoding = pd.read_csv(join(encoding_stim_dir, "encoding_stimuli_split_seed0_swapped.csv"))
+    df_encoding['image_fps'] = parse_image_fullpaths(df_encoding["stimulus_name"], [encoding_stim_dir], arbitrary_format=True)
+    # target_subfolder = sorted(glob.glob(join(acc_stim_root, f"*{subject_id}*_accentuation")))
+    # # List all png files in each subfolder (one level deep)
+    # all_png_files = {}
+    # for subfolder in target_subfolder:
+    #     subfolder_name = os.path.basename(subfolder)
+    #     png_files = glob.glob(join(subfolder, "*.png"))
+    #     # Sort the files to ensure consistent order
+    #     png_files.sort()
+    #     all_png_files[subfolder_name] = png_files
+    #     print(f"Found {len(png_files)} PNG files in {subfolder_name}")
+    #     # Print first few files as example
+    #     for file in png_files[:5]:
+    #         print(f"  - {os.path.basename(file)}")
+    #     if len(png_files) > 5:
+    #         print(f"  - ... and {len(png_files)-5} more files")
+    #     print()
 
-    # Test the function on the file list
-    # Parse all files into a DataFrame
-    png_files_list = sum(all_png_files.values(), [])
-    df_accentuated = parse_accentuated_filenames_to_df(png_files_list)
+    # # Test the function on the file list
+    # # Parse all files into a DataFrame
+    # png_files_list = sum(all_png_files.values(), [])
+    # df_accentuated = parse_accentuated_filenames_to_df(png_files_list)
     # Display info about the DataFrame
-    print(f"DataFrame shape: {df_accentuated.shape}")
+    print(f"DataFrame shape: {df_encoding.shape}")
     print("\nDataFrame columns:")
-    print(df_accentuated.columns.tolist())
+    print(df_encoding.columns.tolist())
     # Show unique models and units
-    print(f"\nUnique models: {df_accentuated['model_name'].nunique()}")
-    print(f"Unique units: {df_accentuated['unit_id'].nunique()}")
-    print(f"Unique image IDs: {df_accentuated['img_id'].nunique()}")
-    print(f"Unique levels: {df_accentuated['level'].nunique()}")
-    print(f"Unique scores: {df_accentuated['score'].nunique()}")
+    # print(f"\nUnique models: {df_accentuated['model_name'].nunique()}")
+    # print(f"Unique units: {df_accentuated['unit_id'].nunique()}")
+    # print(f"Unique image IDs: {df_accentuated['img_id'].nunique()}")
+    # print(f"Unique levels: {df_accentuated['level'].nunique()}")
+    # print(f"Unique scores: {df_accentuated['score'].nunique()}")
 
-    df_accentuated.to_pickle(join(posthoc_dir, f"accentuated_stim_info_{subject_id}.pkl"))
-    df_accentuated.to_csv(join(posthoc_dir, f"accentuated_stim_info_{subject_id}.csv"))
+    # df_accentuated.to_pickle(join(posthoc_dir, f"accentuated_stim_info_{subject_id}.pkl"))
+    # df_accentuated.to_csv(join(posthoc_dir, f"accentuated_stim_info_{subject_id}.csv"))
 
     # %% [markdown]
     # ### Mass produce cross prediction 
@@ -98,7 +100,7 @@ for subject_id in subject_ids:
     # config_pre_chan = [f for f in config_files if chan_pattern in f]
     t0 = time.time()
     pred_resp_dict = {}
-    df_acc_w_pred_resp = df_accentuated.copy()
+    df_encoding_w_pred_resp = df_encoding.copy()
     for config_file in config_files:
         acc_config = yaml.safe_load(open(config_file))
         unit_ids = acc_config['unit_ids']
@@ -109,14 +111,14 @@ for subject_id in subject_ids:
             model, transforms_pipeline, _, _, _ \
                 = construct_predictor_from_config(config_file, device="cuda")
         acc_img_chan_resp = compute_prediction_responses(target_unit_predictor, transforms_pipeline, 
-                                                df_accentuated["filepath"])
+                                                df_encoding["image_fps"])
         pred_resp_dict[(model_name, unit_ids)] = acc_img_chan_resp.cpu().numpy()
-        df_acc_w_pred_resp[f"pred_resp_{model_name}_unit_{unit_ids}"] = acc_img_chan_resp.cpu().numpy()
+        df_encoding_w_pred_resp[f"pred_resp_{model_name}_unit_{unit_ids}"] = acc_img_chan_resp.cpu().numpy()
 
     t1 = time.time()
     print(f"Time taken for all predictors: {t1 - t0} seconds")
-    df_acc_w_pred_resp.to_pickle(join(posthoc_dir, f"accentuated_stim_info_w_pred_resp_{subject_id}.pkl"))
-    pkl.dump(pred_resp_dict, open(join(posthoc_dir, f"pred_resp_dict_{subject_id}.pkl"), "wb"))
+    df_encoding_w_pred_resp.to_pickle(join(posthoc_dir, f"encoding_stim_info_w_pred_resp_{subject_id}.pkl"))
+    pkl.dump(pred_resp_dict, open(join(posthoc_dir, f"encoding_stim_pred_resp_dict_{subject_id}.pkl"), "wb"))
     print(f"Saved to {posthoc_dir} complete!")
 
 
